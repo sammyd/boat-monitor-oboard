f68d370c89239adedbb28ef4463308dc43a83930
Sam Davies <sam.jc.davies@gmail.com>
2013-06-08 11:39:52 +0100

    Creates the low-level data reader classes
    We have a class which organises the scheduling of sample reading, with
    and event loop, a sample reader and a dispatcher.
    
    This commit also implements a random sample reader and contains basic
    tests

---
+++ onboard/input/data_reader.py
@@ -0,0 +1,59 @@
+import random
+
+class DataReader:
+    '''
+    Manages the data sampling. Attaches a sample reader to a provided event loop
+    and then fires the data received onto a dispatcher
+    '''
+
+    def __init__(self, sample_reader, dispatcher, event_loop, sample_time):
+        self._dispatcher = dispatcher
+        self._event_loop = event_loop
+        self._sample_reader = sample_reader
+        self._start_sampling(sample_time)
+
+    def _start_sampling(self, sample_time):
+        # Add to the event loop
+        timer = self._event_loop.timer(0, sample_time, self._timer_callback, 0)
+        # And start the timer
+        timer.start()
+
+    def _timer_callback(self, event_loop):
+        # Get the data
+        data = self._sample_reader.read_sample()
+        # Post data to the dispatcher
+        self._dispatcher.post(data)
+
+
+
+
+class SampleReader:
+    '''
+    Responsible for reading samples from different sources
+    '''
+    def read_sample(self):
+        raise NotImplementedError()
+
+class GPIODataSampleReader(SampleReader):
+    '''
+    This implementation will read samples from the GPIO for the Pi-ADC
+    '''
+
+    def read_sample(self):
+        data = dict()
+        return data
+
+
+class RandomDataSampleReader(SampleReader):
+    '''
+    This implementation generates a set number of random values
+    '''
+
+    def __init__(self, number_samples):
+        self._number_samples = number_samples
+
+    def read_sample(self):
+        data = dict()
+        for i in range(self._number_samples):
+            data[i] = random.random()
+        return data
---
+++ onboard/tests/input/test_data_reader.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import Mock
+
+from onboard.input.data_reader import RandomDataSampleReader
+from onboard.input.data_reader import DataReader
+
+class TestDataReader(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_event_loop = Mock()
+        self.mock_dispatcher = Mock()
+        self.mock_sample_reader = Mock()
+        self.sample_time = 5
+        self.data_reader = DataReader(self.mock_sample_reader, self.mock_dispatcher, self.mock_event_loop, self.sample_time)
+
+    def test_ConstuctionAddsToEventLoop(self):
+        self.mock_event_loop.timer.assert_called_once_with(0, self.sample_time, self.data_reader._timer_callback, 0)
+
+    def test_TimerCallBackAttemptsToDispatch(self):
+        d = dict({'three': 3, 'one': 1, 'two': 2})
+        self.mock_sample_reader.read_sample.return_value = d
+        self.data_reader._timer_callback(self.mock_event_loop)
+        self.mock_dispatcher.post.assert_called_once_with(d)
+
+    def test_TimerCallBackAttemptsToCollectSampleData(self):
+        self.data_reader._timer_callback(self.mock_event_loop)
+        self.mock_sample_reader.read_sample.assert_called()
+
+
+class TestRandomDataSampleReader(unittest.TestCase):
+
+    def test_ReturnsDictionaryOfCorrectSize(self):
+        sample_reader = RandomDataSampleReader(10)
+        data = sample_reader.read_sample()
+        self.assertTrue(len(data) == 10)
+
